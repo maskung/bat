9040084dfa1464a34dc7e7b9a05b5a1ac264ce71
maskung <suphanut@gmail.com>
2017-06-03 04:34:39 +0000

    first commit

---
+++ bat.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+ 
+#define RELAY 0
+#define TRIG 4
+#define ECHO 5
+ 
+void setup() {
+        wiringPiSetup();
+        pinMode(TRIG, OUTPUT);
+        pinMode(ECHO, INPUT);
+        pinMode(RELAY, OUTPUT);
+ 
+        //TRIG pin must start LOW
+        digitalWrite(TRIG, LOW);
+        digitalWrite(RELAY, LOW);
+        delay(30);
+        printf("set up successful\n");
+}
+ 
+int getCM() {
+        //Send trig pulse
+        digitalWrite(TRIG, HIGH);
+        delayMicroseconds(20);
+        digitalWrite(TRIG, LOW);
+ 
+        //Wait for echo start
+        while(digitalRead(ECHO) == LOW);
+ 
+        //Wait for echo end
+        long startTime = micros();
+        while(digitalRead(ECHO) == HIGH);
+        long travelTime = micros() - startTime;
+ 
+        //Get distance in cm
+        int distance = travelTime / 58;
+ 
+        return distance;
+}
+ 
+int main(void) {
+        //cat fly pass 
+        int distant;
+
+        setup();
+ 
+        while (1) {
+
+            //find distant
+            distant  = getCM();
+
+            // if objects are in range not more than 100 centimetre
+            if (distant >= 20 && distant <= 100) {
+                digitalWrite(RELAY, HIGH);
+            }
+            printf("Distance: %dcm\n", distant);
+            delay(1000);
+            digitalWrite(RELAY, LOW);
+
+
+        }
+ 
+        return 0;
+}
+
